cb5b5bcf2ca139f9bb4ee4e60849ba986e7b4c6a
arctickirillas <kirakle1000000@rambler.ru>
2015-12-16 15:21:12 +0300

    Initial commit 16.12

--- quantify.py
+++ quantify.py
@@ -10,22 +10,22 @@ from sklearn.svm import SVC
 from sklearn.multiclass import OneVsRestClassifier as mc
 from sklearn import metrics
 from sklearn import linear_model
+import numpy as np
 import sys
 
 #
-file=open('dump','rb')
-dump = p.load(file)
-predicted = dump.toarray()
-file.close()
-# print (dump)
-
+# file=open('dump','rb')
+# dump = p.load(file)
+# predicted = dump.toarray()
+# file.close()
+# # print (dump)
 #
-file=open('prob','rb')
-prob = p.load(file)
-file.close()
+# #
+# file=open('prob','rb')
+# prob = p.load(file)
+# file.close()
 # print prob
 
-# Как получить 2510 и 88??
 
 def classifyAndCount(predicted, probability = []):
     quantity = [0.]*88
@@ -52,26 +52,23 @@ def CCforDouble(predicted, probability = []):
     quantityNeg, quantityPos = 0.,0.
     probQuantityNeg = 0.0
     probQuantityPos = 0.0
+    array = [0.0]*2
     if len(probability) == 0:
-        print 'Classify And Count:'
+        # print 'Classify And Count:'
         for i in range(len(predicted)):
-            if str(predicted[i]) == 'negative':
+            if str(predicted[i]) == 'negative' or str(predicted[i]) == '-1' or str(predicted[i]) == '0':
                 quantityNeg += 1
-            elif str(predicted[i]) == 'positive':
+            elif str(predicted[i]) == 'positive' or str(predicted[i]) ==  '1':
                 quantityPos += 1
         quantityNeg = quantityNeg/len(predicted)
         quantityPos = quantityPos/len(predicted)
-        print quantityPos,quantityNeg
+        return quantityPos,quantityNeg
     else:
-        print 'Probabilistic Classify And Count :'
-        for i in range(len(predicted)):
-            if predicted[i] == 'negative':
-                    probQuantityNeg += probability[i]
-            elif predicted[i] == 'positive':
-                    probQuantityPos += probability[i]
-        probQuantityNeg = probQuantityNeg/len(predicted)
-        probQuantityPos = probQuantityPos/len(predicted)
-        print probQuantityPos,probQuantityNeg
+        # print 'Probabilistic Classify And Count :'
+        probability =  np.asarray(probability).transpose()
+        for j in range(len(probability)):
+                array[j] = np.average(probability[j])
+        return array[1],array[0]
 
 # classifyAndCount(predicted)
 # classifyAndCount(predicted,prob)
